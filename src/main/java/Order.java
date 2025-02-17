b48fcbc33d3bc181ff4151e16cc3c84a95807c76
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-17 16:44:13 +0100

    add orderDate to Order. modify all usages.

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,8 +1,10 @@
+import java.time.Instant;
 import java.util.List;
 
 public record Order(
         String id,
         List<Product> products,
-        OrderStatus orderStatus
+        OrderStatus orderStatus,
+        Instant orderDate
 ) {
 }
